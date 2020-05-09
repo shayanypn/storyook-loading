9949d12ca2de3aa35ce4421baebde803c9164424
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-09 02:33:41 +0000

    Deploy shayanypn/storyook-loading to github.com/shayanypn/storyook-loading.git:gh-pages

---
+++ precache-manifest.78e942f50921de17592faaff6d6827e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762aba282b81b9f0174b422cbe76a536",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.791d216964c7e39bd6b0.bundle.js"
+  },
+  {
+    "url": "runtime~main.791d216964c7e39bd6b0.bundle.js"
+  },
+  {
+    "revision": "78e2b47b3e14f400d4fe",
+    "url": "static/css/main.f734b8fc.chunk.css"
+  },
+  {
+    "url": "vendors~main.791d216964c7e39bd6b0.bundle.js"
+  },
+  {
+    "url": "vendors~main.791d216964c7e39bd6b0.bundle.js.LICENSE.txt"
+  }
+]);
